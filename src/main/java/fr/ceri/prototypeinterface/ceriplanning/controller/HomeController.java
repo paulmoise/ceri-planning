a4bf3fa1880d21d76133876bbb78091ddf68edb2
paulmoise <m.p.gangbadja@gmail.com>
2024-03-11 13:43:43 +0100

    design month view v1

--- src/main/java/fr/ceri/prototypeinterface/ceriplanning/controller/HomeController.java
+++ src/main/java/fr/ceri/prototypeinterface/ceriplanning/controller/HomeController.java
@@ -1,8 +1,182 @@
 package fr.ceri.prototypeinterface.ceriplanning.controller;
 
-//import java.
-public class HomeController {
+import fr.ceri.prototypeinterface.ceriplanning.model.CalendarActivity;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
-    String[] weekAbbr = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+import java.net.URL;
+import java.time.ZonedDateTime;
+import java.util.*;
+
+public class HomeController implements Initializable {
+
+    @FXML
+    private AnchorPane weekcalendar;
+
+    // Initialize the GridPane
+    private GridPane gridPane = new GridPane();
+
+    // Array for weekdays starting from Monday
+    String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+
+    ZonedDateTime dateFocus;
+    ZonedDateTime today;
+
+    @FXML
+    private Text year;
+
+    @FXML
+    private Text month;
+
+    @FXML
+    private FlowPane monthCalendar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dateFocus = ZonedDateTime.now();
+        System.out.println(dateFocus.getYear());
+        today = ZonedDateTime.now();
+        drawCalendar();
+    }
+
+    @FXML
+    void backOneMonth(ActionEvent event) {
+        dateFocus = dateFocus.minusMonths(1);
+        monthCalendar.getChildren().clear();
+        drawCalendar();
+    }
+
+    @FXML
+    void forwardOneMonth(ActionEvent event) {
+        dateFocus = dateFocus.plusMonths(1);
+        monthCalendar.getChildren().clear();
+        drawCalendar();
+    }
+
+
+    private void drawCalendar() {
+        System.out.println(dateFocus.getYear());
+        year.setText(String.valueOf(dateFocus.getYear()));
+        month.setText(String.valueOf(dateFocus.getMonth()));
+        double calendarWidth = monthCalendar.getPrefWidth();
+        double calendarHeight = monthCalendar.getPrefHeight();
+        double strokeWidth = 1;
+        double spacingH = monthCalendar.getHgap();
+        double spacingV = monthCalendar.getVgap();
+
+        //List of activities for a given month
+        Map<Integer, List<CalendarActivity>> calendarActivityMap = getCalendarActivitiesMonth(dateFocus);
+
+        int monthMaxDate = dateFocus.getMonth().maxLength();
+        //Check for leap year
+        if (dateFocus.getYear() % 4 != 0 && monthMaxDate == 29) {
+            monthMaxDate = 28;
+        }
+        int dateOffset = ZonedDateTime.of(dateFocus.getYear(), dateFocus.getMonthValue(), 1, 0, 0, 0, 0, dateFocus.getZone()).getDayOfWeek().getValue();
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                StackPane stackPane = new StackPane();
+
+                Rectangle rectangle = new Rectangle();
+                rectangle.setFill(Color.TRANSPARENT);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setStrokeWidth(strokeWidth);
+                double rectangleWidth = (calendarWidth / 7) - strokeWidth - spacingH;
+                rectangle.setWidth(rectangleWidth);
+                double rectangleHeight = (calendarHeight / 6) - strokeWidth - spacingV;
+                rectangle.setHeight(rectangleHeight);
+                stackPane.getChildren().add(rectangle);
+
+                int calculatedDate = (j + 1) + (7 * i);
+                if (calculatedDate > dateOffset) {
+                    int currentDate = calculatedDate - dateOffset;
+                    if (currentDate <= monthMaxDate) {
+                        Text date = new Text(String.valueOf(currentDate));
+                        double textTranslationY = -(rectangleHeight / 2) * 0.75;
+                        date.setTranslateY(textTranslationY);
+                        stackPane.getChildren().add(date);
+
+                        List<CalendarActivity> calendarActivities = calendarActivityMap.get(currentDate);
+                        if (calendarActivities != null) {
+                            createCalendarActivity(calendarActivities, rectangleHeight, rectangleWidth, stackPane);
+                        }
+                    }
+                    if (today.getYear() == dateFocus.getYear() && today.getMonth() == dateFocus.getMonth() && today.getDayOfMonth() == currentDate) {
+                        rectangle.setStroke(Color.BLUE);
+                    }
+                }
+                monthCalendar.getChildren().add(stackPane);
+            }
+        }
+    }
+
+
+    private void createCalendarActivity(List<CalendarActivity> calendarActivities, double rectangleHeight, double rectangleWidth, StackPane stackPane) {
+        VBox calendarActivityBox = new VBox();
+        for (int k = 0; k < calendarActivities.size(); k++) {
+            if (k >= 2) {
+                Text moreActivities = new Text("...");
+                calendarActivityBox.getChildren().add(moreActivities);
+                moreActivities.setOnMouseClicked(mouseEvent -> {
+                    //On ... click print all activities for given date
+                    System.out.println(calendarActivities);
+                });
+                break;
+            }
+            Text text = new Text(calendarActivities.get(k).getClientName() + ", " + calendarActivities.get(k).getDate().toLocalTime());
+            calendarActivityBox.getChildren().add(text);
+            text.setOnMouseClicked(mouseEvent -> {
+                //On Text clicked
+                System.out.println(text.getText());
+            });
+        }
+        calendarActivityBox.setTranslateY((rectangleHeight / 2) * 0.20);
+        calendarActivityBox.setMaxWidth(rectangleWidth * 0.8);
+        calendarActivityBox.setMaxHeight(rectangleHeight * 0.65);
+        calendarActivityBox.setStyle("-fx-background-color:GRAY");
+        stackPane.getChildren().add(calendarActivityBox);
+    }
+
+
+    private Map<Integer, List<CalendarActivity>> createCalendarMap(List<CalendarActivity> calendarActivities) {
+        Map<Integer, List<CalendarActivity>> calendarActivityMap = new HashMap<>();
+
+        for (CalendarActivity activity : calendarActivities) {
+            int activityDate = activity.getDate().getDayOfMonth();
+            if (!calendarActivityMap.containsKey(activityDate)) {
+                calendarActivityMap.put(activityDate, List.of(activity));
+            } else {
+                List<CalendarActivity> OldListByDate = calendarActivityMap.get(activityDate);
+
+                List<CalendarActivity> newList = new ArrayList<>(OldListByDate);
+                newList.add(activity);
+                calendarActivityMap.put(activityDate, newList);
+            }
+        }
+        return calendarActivityMap;
+    }
+
+    private Map<Integer, List<CalendarActivity>> getCalendarActivitiesMonth(ZonedDateTime dateFocus) {
+        List<CalendarActivity> calendarActivities = new ArrayList<>();
+        int year = dateFocus.getYear();
+        int month = dateFocus.getMonth().getValue();
+
+        Random random = new Random();
+        for (int i = 0; i < 50; i++) {
+            ZonedDateTime time = ZonedDateTime.of(year, month, random.nextInt(27) + 1, 16, 0, 0, 0, dateFocus.getZone());
+            calendarActivities.add(new CalendarActivity(time, "Hans", 111111));
+        }
+
+        return createCalendarMap(calendarActivities);
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module fr.ceri.prototypeinterface.ceriplanning {
     requires org.kordamp.bootstrapfx.core;
 
     opens fr.ceri.prototypeinterface.ceriplanning to javafx.fxml;
+    opens fr.ceri.prototypeinterface.ceriplanning.controller to javafx.fxml;
     exports fr.ceri.prototypeinterface.ceriplanning;
     exports fr.ceri.prototypeinterface.ceriplanning.controller;
 
