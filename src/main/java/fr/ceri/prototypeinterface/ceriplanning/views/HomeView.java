7e589414b7ae211e34c505c360cd02f4098e3826
paulmoise <m.p.gangbadja@gmail.com>
2024-03-11 09:13:01 +0100

    layout design

---
+++ src/main/java/fr/ceri/prototypeinterface/ceriplanning/views/HomeView.java
@@ -0,0 +1,11 @@
+package fr.ceri.prototypeinterface.ceriplanning.views;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HomeView extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
